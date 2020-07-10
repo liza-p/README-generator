d83b6e1ed9c197b89ed6c4d57154cf31d8ec6683
liza p <liyourlp@gmail.com>
2020-07-10 16:28:11 -0700

    add list of questions

--- Develop/index.js
+++ Develop/index.js
@@ -5,26 +5,101 @@ const inquirer = require("inquirer");
 
 const questions = [
     {
+    type: "input",
+    name: "username",
+    message: "Provide github username"
+    },
+    {
     type: 'input',
-    message: "Enter your README Title",
+    message: "Enter your project Title",
     name: "title"
     },
     {
     type: 'input',
-    message: "Enter your README Description",
+    message: "Enter your project Description",
     name: "description"
     },
+    {
+      type: 'input',
+      message: "Enter link to the deployed application",
+      name: "deployedLink"
+      },
+    {
+    type: 'input',
+    message: "Provide installation steps",
+    name: "installation"
+    },
+    {
+    type: 'input',
+    message: "Provide usage instructions",
+    name: "usage"
+    },
+    {
+    type: "list",
+    name: "license",
+    message: "Select appropriate license",
+    choices: [
+          "MIT",
+          "ISC",
+          "GPL"
+      ]
+   },
+   {
+   type: "input",
+   name: "contributing",
+   message: "Enter instructions for contributors"
+   },
+   {
+    type: "input",
+    name: "test",
+    message: "Enter testing instructions"
+   }
+
+
+
+
     
     
 
 ];
     inquirer
   .prompt(questions)
-  .then(answers => {
-    console.log(answers);
-    writeToFile("README.md", 
-    `## ${answers.title}
-${answers.description}`)
+  .then(userInput => {
+    console.log(userInput);
+    writeToFile("TheREADME.md", 
+    `## ${userInput.title}
+${userInput.description}
+ ## Link to the deployed application
+ -${userInput.deployedLink}
+
+## Table of Contents
+* [Installation](##Installation)
+* [Usage](##Usage)
+* [License](##License)
+* [Contributing](##Contributing)
+* [Tests](##Tests)
+* [Author](##Author)
+
+## Installation 
+- ${userInput.installation}
+
+## Usage
+- ${userInput.usage}
+
+## License
+![License](https://img.shields.io/badge/license-${userInput.license}-blueviolet)
+
+## Contributing 
+- ${userInput.contributing}
+
+## Tests
+- ${userInput.test}
+
+## Author
+- [${userInput.username}](https://github.com/${userInput.username})
+
+`
+    );
   })
   .catch(error => {
     if(error.isTtyError) {
